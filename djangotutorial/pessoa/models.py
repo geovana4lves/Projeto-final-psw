badd15079ca089041bb97b3aac8cf21f8c7508ac
Geovana_Alves <geovanaalvesgbi4@gmail.com>
2026-08-18 12:41:42 -0300

    .

--- djangotutorial/pessoa/models.py
+++ djangotutorial/pessoa/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
-class Pessoa(models.Model):
+class Pessoa(User):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     cpf = models.CharField(max_length=14)
